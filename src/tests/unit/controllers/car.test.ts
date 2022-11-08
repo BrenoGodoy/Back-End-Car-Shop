eea43ddcf3d62a55f88144b99261185d51ae9676
Breno Godoy <godoyy25z@gmail.com>
2022-11-08 02:26:13 -0300

    test: 30% da camada de controllers

--- src/tests/unit/controllers/car.test.ts
+++ src/tests/unit/controllers/car.test.ts
@@ -1,23 +1,43 @@
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// import constro from '../../../services/cars';
-// import Car from '../../../models/cars';
-// import { carWithId, request } from '../../mocks/mocks';
+import Sinon, * as sinon from 'sinon';
+import chai from 'chai';
+import Car from '../../../models/cars';
+import { carWithId, request } from '../../mocks/mocks';
+import ServiceCars from '../../../services/cars';
+import ControllerCars from '../../../controllers/cars';
+import { Request, Response } from 'express';
 
-// const { expect } = chai;
-// const car = new Car();
-// const service = new ServiceCars(new Car());
+const { expect } = chai;
+const car = new Car();
+const service = new ServiceCars(car);
+const controller = new ControllerCars(service);
 
-// describe('Car Controllers', () => {
+describe('CONTROLLER', () => {
 
-//   afterEach(()=> sinon.restore());
+  const req = {} as Request;
+  const res = {} as Response;
 
-//   it('create', async () => {
-//     sinon
-//       .stub(service, 'registerNewCar')
-//       .resolves({ code: 201, response: carWithId });
-//     const response = await service.registerNewCar(request);
-//     expect(response.code).to.be.equal(201);
-//     expect(response.response).to.be.deep.equal(carWithId);
-//   });
-// });
+  beforeEach(() => {
+    res.status = Sinon.stub().returns(res);
+    res.json = Sinon.stub().returns(res);
+  });
+
+  afterEach(()=> sinon.restore());
+  describe('CREATE', () => {
+    it('Success Create', async () => {
+      sinon
+        .stub(service, 'registerNewCar')
+        .resolves({ code: 201, response: carWithId });
+      await controller.registerNewCar(req, res);
+      expect((res.status as Sinon.SinonStub).calledWith(201)).to.be.true;
+    });
+  });
+  describe('READ', () => {
+    it('Success Read', async () => {
+      sinon
+        .stub(service, 'getAll')
+        .resolves({ code: 200, response: [carWithId] });
+      await controller.getAll(req, res);
+      expect((res.status as Sinon.SinonStub).calledWith(200)).to.be.true;
+    });
+  });
+});
