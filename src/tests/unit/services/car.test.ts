c8445cd30d976f477cbafff57adecb08eb9a7c21
Breno Godoy <godoyy25z@gmail.com>
2022-11-08 02:13:34 -0300

    test: 60% da camada de service

--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -6,36 +6,64 @@ import { carWithId, request } from '../../mocks/mocks';
 
 const { expect } = chai;
 
-describe('Car Service', async () => {
+const car = new Car();
+const service = new ServiceCars(car);
 
-  const car = new Car();
-  const service = new ServiceCars(new Car());
-
+describe('SERVICE', () => {
   afterEach(()=> sinon.restore());
+  describe('CREATE', async () => {
+  
+    it('Succes Create', async () => {
+      sinon.stub(car, 'create').resolves(carWithId);
+      const response = await service.registerNewCar(request);
+      expect(response.code).to.be.equal(201);
+      expect(response.response).to.be.deep.equal(carWithId);
+    });
+  });
+  describe('READ', async () => {
+    it('Success Read', async () => {
+      const cars = [carWithId];
 
-  it('create', async () => {
-    sinon
-      .stub(service, 'registerNewCar')
-      .resolves({ code: 201, response: carWithId });
-    const response = await service.registerNewCar(request);
-    expect(response.code).to.be.equal(201);
-    expect(response.response).to.be.deep.equal(carWithId);
+      sinon
+        .stub(car, 'read')
+        .resolves(cars);
+      const response = await service.getAll();
+      expect(response.code).to.be.equal(200);
+      expect(response.response).to.be.deep.equal([carWithId]);
+    });
   });
-  it('read', async () => {
-    sinon
-      .stub(service, 'getAll')
-      .resolves({ code: 200, response: [carWithId] });
-    const response = await service.getAll();
-    expect(response.code).to.be.equal(200);
-    expect(response.response).to.be.deep.equal([carWithId]);
-  });
-  // it('readOne', async () => {
-  //   sinon
-  //     .stub(car, 'read')
-  //     .resolves([carWithId]);
-  //   const response = await service.getOne('635495a97e1e13e479011e2d');
-  //   expect(response.code).to.be.eq(200);
-  //   expect(response.response).to.be.an('array');
-  //   expect(response.response).to.be.deep.equal([carWithId]);
-  // });
+    describe('READ ONE', async () => {
+      it('Succes ReadOne', async () => {
+        sinon.stub(car, 'readOne').resolves(carWithId);
+        const response = await service.getOne('4edd40c86762e0fb12000003');
+        expect(response.code).to.be.eq(200);
+        expect(response.response).to.be.deep.equal(carWithId);
+      });
+      it('Fail ReadOne', async () => {
+        let message;
+        try {
+          const response = await car.readOne('1');
+        } catch(err: any) {
+          message = err.message;
+        }
+        expect(message).to.be.deep.equal('InvalidMongoId');
+      });
+    });
+    describe('READ ONE', async () => {
+      it('Succes Update', async () => {
+        sinon.stub(car, 'update').resolves(carWithId);
+        const response = await service.update('4edd40c86762e0fb12000003', carWithId);
+        expect(response.code).to.be.eq(200);
+        expect(response.response).to.be.deep.equal(carWithId);
+      });
+      it('Fail Update', async () => {
+        let message;
+        try {
+          const response = await car.update('1', carWithId);
+        } catch(err: any) {
+          message = err.message;
+        }
+        expect(message).to.be.deep.equal('InvalidMongoId');
+      });
+    });
 });
